20fc64ae86ea901df015862d69c10ae3171139bc
Francine Santos <francinesanto@gmail.com>
2024-01-08 14:24:11 -0300

    add key

--- src/components/ListaDeEstudantes/index.jsx
+++ src/components/ListaDeEstudantes/index.jsx
@@ -5,8 +5,9 @@ const ListaDeEstudantes = () => {
     return (
         <div className="wrapper">
             {
-             estudantes.map((estudante) => (
+             estudantes.map((estudante,index) => (
                 <Card 
+                    key = {index}
                     nome ={estudante.nome}
                     fotoUrl ={estudante.foto}
                     comidaN = {estudante.comidaQueNaoGosta}
