7bffa1ab68b39b019d8fdd2e32fed32088c5c6c3
Francine Santos <francinesanto@gmail.com>
2024-01-08 14:32:51 -0300

    ajustando caixa de texto

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -10,9 +10,10 @@ const Form = () => {
       <form action="">
          <Titulo texto='Digite os dados:'/>
          <CaixaDeTexto label= 'Nome' placeholder ='Digite o seu nome'/>
-         <CaixaDeTexto label= 'Cargo' placeholder='Digite o seu cargo' />
-         <CaixaDeTexto label= 'Imagem' placeholder='Digite o endereço da sua imagem' />
-         <CampoSelect label= 'Time' opcoes={cargos}/>
+         <CaixaDeTexto label= 'Foto' placeholder='Digite a url da sua foto' />
+         <CaixaDeTexto label= 'Comida que você não gosta' placeholder='Digite a comida que você não gosta' />
+         <CaixaDeTexto label= 'Experiência Positiva' placeholder='Digite uma experiência positiva' />
+         <CampoSelect label= 'Experiência positiva' opcoes={cargos}/>
          <Botao label = 'Criar card' />
       </form>
     </section>
