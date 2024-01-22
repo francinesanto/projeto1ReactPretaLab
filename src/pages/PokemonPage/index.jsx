42af02c91e1237daa6d0473de22d232e67ad6b93
Francine Santos <francinesanto@gmail.com>
2024-01-22 11:35:22 -0300

    consumindo PokeApi

--- src/pages/PokemonPage/index.jsx
+++ src/pages/PokemonPage/index.jsx
@@ -1,8 +1,58 @@
+import { useState } from "react"
 import CardPokemon from "../../components/CardPokemon"
-
+import axios from "axios"
+import { useEffect } from "react"
+import "./styles.css"
 const PokemonPage = () => {
+  const [refPokemons, setRefPokemons] = useState([])
+  const [listaPokemons, setListaPokemons] = useState([])
+  
+  const pegar100ReferenciasPokemons = async () => {
+    try{
+      const resposta = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100")
+      setRefPokemons(resposta.data.results)
+      
+    } catch (error) {
+      console.error("Erro ao buscar as referecias dos pokemons", error)
+    }
+  }
+
+  useEffect(() => {
+    pegar100ReferenciasPokemons()
+  },[])
+
+
+  const pegarListaDePokemons = async () => {
+    const listaTemporaria = []
+    for (const referencia of refPokemons){
+      try {
+        const resposta = await axios.get(referencia.url)
+        listaTemporaria.push(resposta.data)
+      } catch (error) {
+        console.error("Erro ao buscar o pokemon", error)
+      }
+    }
+   
+    setListaPokemons(listaTemporaria)
+  }
+
+  useEffect(() => {
+    pegarListaDePokemons()
+  }, [refPokemons])
+
+
    return (
-     <CardPokemon />
+      <div className="pokemonContainer">
+       {listaPokemons.map((pokemon) => (
+        <CardPokemon
+         nome = {pokemon.name}
+         foto = {pokemon.sprites.front_default}
+         id = {pokemon.id} 
+         url = {pokemon.forms[0].url}
+         />
+       ))}
+      </div>
+    
    )
     }
 
