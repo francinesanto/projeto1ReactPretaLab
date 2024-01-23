85b21b18f39020f4cc7c29db09982d45fcf6e34e
Francine Santos <francinesanto@gmail.com>
2024-01-23 12:16:44 -0300

    primeira finalização de busca de cep

--- src/components/FormCep/index.jsx
+++ src/components/FormCep/index.jsx
@@ -1,31 +1,31 @@
-import { useState } from "react"
+
 import Botao from "../Botao"
 import "./styles.css"
 
 
-const FormCep = () => {
+const FormCep = (cep ,rua, cidade, estado, latitude, longitude) => {
     return (
         
             <section className="form">
                <h1>Consulte seu Cep</h1>
                <p>via <a href="https://brasilapi.com.br/">Brasil API</a></p>
-               <input type="text" name="cep"  placeholder="Digite seu CEP" /> 
-               <Botao texto="Buscar" />
+               <input type="text" name="cep"  placeholder="Digite seu CEP" value={cep} onChange={(e) => setCep(e.target.value)}/> 
+               <Botao texto="Buscar" acao={obterDadosCep}/>
                 <form action="" className="formCep">
                    <label htmlFor="rua">Rua</label>
-                   <input type="text"   />
+                   <input type="text"  value={rua} />
 
                    <label htmlFor="cidade">Cidade</label>
-                   <input type="text" />
+                   <input type="text"  value={cidade}/>
                    
                    <label htmlFor="estado">Estado</label>
-                   <input type="text"  />
+                   <input type="text" value={estado}  />
 
                    <label htmlFor="latitude">Latitude</label>
-                   <input type="text"  />
+                   <input type="text" value={latitude}  />
 
                    <label htmlFor="longitude">Longitude</label>
-                   <input type="text" />
+                   <input type="text" value={longitude} />
                 </form>
             </section>   
         
--- src/pages/CepPage/index.jsx
+++ src/pages/CepPage/index.jsx
@@ -1,25 +1,46 @@
 import { useState } from "react"
-import FormCep from "../../components/FormCep"
 import axios from "axios"
 
 const CepPage = () => {
-    const [cep, setCep] = useState('')
-
-    const verificarCep = async () => {
-        try {
-            const resposta = await axios.get('https://brasilapi.com.br/api/cep/v1/{cep}')
-            setCep(resposta.data)
-            console.log(resposta)
-        } catch (error) {
-            console.error("Todos os serviços de cep retornaram erro", error)
-        }
-    }
-
-
+    const [cep, setCep] = useState('');
+    const [dadosCep, setDadosCep] = useState(null);
+  
+    const obterDadosCep = async () => {
+      try {
+        const resposta = await axios.get(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+        setDadosCep(resposta.data);
+        console.log(resposta)
+      } catch (erro) {
+        console.error('Erro na requisição:', erro);
+      }
+    };
+  
     return (
-          
-         <FormCep/>
-    )
+      <div>
+        <label>
+          Digite o CEP:
+          <input
+            type="text"
+            value={cep}
+            onChange={(e) => setCep(e.target.value)}
+          />
+        </label>
+        <button onClick={obterDadosCep}>Buscar</button>
+  
+        {dadosCep && (
+          <div>
+            <h2>Dados do CEP:</h2>
+            <p>CEP: {dadosCep.cep}</p>
+            <p>Logradouro: {dadosCep.street}</p>
+            <p>Bairro: {dadosCep.neighborhood}</p>
+            <p>Cidade: {dadosCep.city}</p>
+            <p>Estado: {dadosCep.state}</p>
+            <p>Longitude: {dadosCep.location.coordinates.longitude}</p>
+            <p>Latitude: {dadosCep.location.coordinates.latitude}</p>
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default CepPage
