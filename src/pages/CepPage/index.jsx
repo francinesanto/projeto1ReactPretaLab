66824331b198e45fe3373fce18fe21a4cc868c42
Francine Santos <francinesanto@gmail.com>
2024-01-25 17:32:42 -0300

    estilizando com material ui

--- src/pages/CepPage/index.jsx
+++ src/pages/CepPage/index.jsx
@@ -1,46 +1,49 @@
 import { useState } from "react"
 import axios from "axios"
-
+import  Box  from "@mui/material/Box"
+import  Stack  from "@mui/material/Stack"
+import  Typography  from "@mui/material/Typography"
+import  Link  from "@mui/material/Link"
+import  TextField  from "@mui/material/TextField"
+import  Button  from "@mui/material/Button"
 const CepPage = () => {
-    const [cep, setCep] = useState('');
-    const [dadosCep, setDadosCep] = useState(null);
+  const [cep, setCep] = useState('')
+  const [dadosCep, setDadosCep] = useState(null)
   
-    const obterDadosCep = async () => {
-      try {
-        const resposta = await axios.get(`https://brasilapi.com.br/api/cep/v2/${cep}`);
-        setDadosCep(resposta.data);
-        console.log(resposta)
-      } catch (erro) {
-        console.error('Erro na requisição:', erro);
-      }
-    };
-  
-    return (
-      <div>
-        <label>
-          Digite o CEP:
-          <input
-            type="text"
-            value={cep}
-            onChange={(e) => setCep(e.target.value)}
-          />
-        </label>
-        <button onClick={obterDadosCep}>Buscar</button>
-  
-        {dadosCep && (
-          <div>
-            <h2>Dados do CEP:</h2>
-            <p>CEP: {dadosCep.cep}</p>
-            <p>Logradouro: {dadosCep.street}</p>
-            <p>Bairro: {dadosCep.neighborhood}</p>
-            <p>Cidade: {dadosCep.city}</p>
-            <p>Estado: {dadosCep.state}</p>
-            <p>Longitude: {dadosCep.location.coordinates.longitude}</p>
-            <p>Latitude: {dadosCep.location.coordinates.latitude}</p>
-          </div>
-        )}
-      </div>
-    );
+  const obterDadosCep = async () => {
+    try {
+      const resposta = await axios.get(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+      setDadosCep(resposta.data);
+      console.log(resposta)
+    } catch (erro) {
+      console.error('Erro na requisição:', erro);
+    }
+  };
+
+   return(
+    <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column' marginTop='15px' width='100vw' gap={2}p>
+      <Stack  alignItems='center' gap={1}>
+          <Typography variant="h4">Consulte seu CEP</Typography>
+          <Link href='https://brasilapi.com.br' target='_blank' underline="always"> via Brasil API</Link>
+      </Stack>
+
+      <Stack alignItems='center' gap={1} direction="row">
+          <TextField id="idCep" placeholder="00000000" required inputProps={{maxLength: 8}} value={cep} onChange={(event) => setCep(event.target.value)}></TextField>
+          <Button variant="contained" onClick={obterDadosCep} > Buscar </Button>
+      </Stack>
+
+    {dadosCep && <Stack width='50vh' gap={2} marginTop={1}>
+          <TextField label="Rua" fullWidth readOnly value={dadosCep.street}/>
+          <TextField label="Cidade" fullWidth readOnly value={dadosCep.city}/>
+          <TextField label="Estado" fullWidth readOnly value={dadosCep.state}/>
+          <TextField label="Latitude" fullWidth readOnly value={dadosCep.location.coordinates.latitude}/>
+          <TextField label="Longitude" fullWidth readOnly value={dadosCep.location.coordinates.longitude}/>
+      </Stack>
+    }
+    </Box>
+    
+
+   )
 }
 
 export default CepPage
