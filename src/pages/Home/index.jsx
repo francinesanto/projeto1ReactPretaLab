50c49f08fe66aaba5cc588705c5ae223bbbcc981
Francine Santos <francinesanto@gmail.com>
2024-01-29 14:15:50 -0300

    primeira finalização da pagina home com sobre mim

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -17,7 +17,46 @@ const Home = () => {
        
       </main>
 
-   
+      <section id='sobreMim'>
+          <div className='titulo'>
+            <h2>sobre mim</h2>
+          </div>
+          <div className='perfil'>
+            <img src="/fotoPerfil.png" alt="" />
+            <p>Profissional em transição de carreira, tive uma primeira oportunidade com desenvolvimento <strong> backend</strong> em projetos desenvolvidos em node JS/Typescript, no momento me qualifico para trabalhar com <strong>REACT</strong>. Trabalhei por mais de 8 anos em eventos corporativos e vendas, tendo facilidade em comunicação, boa adaptação para mudanças e para solução de problemas.
+            </p>
+          </div>
+          <div className='detalhe2'>
+        <img src="/detalhe2.png" alt=""  />
+        </div>
+      </section>
+
+      <section id='descricaoCurso'>   
+        <div className='titulo'>
+              <h2>Ciclo formativo PretaLab</h2>
+        </div>
+
+      <div id='pinsCont'>
+        <div className='pin'>
+          <h3>Html</h3>
+        </div>
+        <div className='pin'>
+          <h3>Css</h3> 
+        </div>
+        <div className='pin'>
+          <h3>Javascript</h3>
+        </div>
+        <div className='pin'>
+          <h3>React</h3>
+        </div>
+        <div className='pin'>
+          <h3>Testes com Jest</h3>
+        </div>
+      </div>
+      <div className='divLink'>
+       <a href="/sobre">Sobre a PretaLab</a>
+      </div>
+      </section>
     </>
   )
 
