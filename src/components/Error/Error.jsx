03e900dce0ec3e5393c3121643b1a992626f6787
Francine Santos <francinesanto@gmail.com>
2024-01-29 15:48:22 -0300

    corrigindo main do erro

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -4,7 +4,7 @@ import './styles.css'
 const Error = () => {
     return (
         <>
-        <main>
+        <main className='mainErro'>
         <h2>Página não encontrada, aprenda React e desenvolva uma =D</h2>
         <div className="img">
             <img src="/erro-404.png" alt="" />
