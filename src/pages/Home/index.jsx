43b604bc6da714f1540411c2f18194e5f75cf548
Francine Santos <francinesanto@gmail.com>
2024-01-29 14:36:45 -0300

    alinhando titulo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -28,35 +28,35 @@ const Home = () => {
             </p>
           </div>
           <div className='detalhe2'>
-        <img src="/detalhe2.png" alt=""  />
-        </div>
+            <img src="/detalhe2.png" alt=""  />
+          </div>
       </section>
 
       <section id='descricaoCurso'>   
-        <div className='titulo'>
-              <h2>Ciclo formativo PretaLab</h2>
+        <div className='desc'>
+            <h2>Ciclo formativo PretaLab</h2>
         </div>
 
-      <div id='pinsCont'>
-        <div className='pin'>
-          <h3>Html</h3>
+        <div id='pinsCont'>
+          <div className='pin'>
+            <h3>Html</h3>
+          </div>
+          <div className='pin'>
+            <h3>Css</h3> 
+          </div>
+          <div className='pin'>
+            <h3>Javascript</h3>
+          </div>
+          <div className='pin'>
+            <h3>React</h3>
+          </div>
+          <div className='pin'>
+            <h3>Testes com Jest</h3>
+          </div>
         </div>
-        <div className='pin'>
-          <h3>Css</h3> 
+        <div className='divLink'>
+        <Link href="/sobre">Sobre a PretaLab</Link>
         </div>
-        <div className='pin'>
-          <h3>Javascript</h3>
-        </div>
-        <div className='pin'>
-          <h3>React</h3>
-        </div>
-        <div className='pin'>
-          <h3>Testes com Jest</h3>
-        </div>
-      </div>
-      <div className='divLink'>
-       <Link href="/sobre">Sobre a PretaLab</Link>
-      </div>
       </section>
     </>
   )
