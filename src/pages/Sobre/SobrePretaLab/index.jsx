4db24541b09b61e2bcab69936f34fa3fddcc645f
Francine Santos <francinesanto@gmail.com>
2024-01-29 16:39:58 -0300

    deletando form não usado

--- src/pages/Sobre/SobrePretaLab/index.jsx
+++ src/pages/Sobre/SobrePretaLab/index.jsx
@@ -1,14 +1,7 @@
 import "./styles.css"
 
 const SobrePretaLab = () => {
-    return(
-        <main className="mainSobrePretaLab">
-            <div >
-                <img src="/logopreta.jpg" alt=""  className="imgPreta"/>
-            </div>
-           
-        </main>
-    )
+
 }
 
 export default SobrePretaLab
