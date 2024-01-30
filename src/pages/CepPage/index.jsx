9ea9f4349e226a755276b4ec917b36deac17dac9
Francine Santos <francinesanto@gmail.com>
2024-01-30 09:43:09 -0300

    utilizando theme especifico

--- src/pages/CepPage/index.jsx
+++ src/pages/CepPage/index.jsx
@@ -6,7 +6,22 @@ import  Typography  from "@mui/material/Typography"
 import  Link  from "@mui/material/Link"
 import  TextField  from "@mui/material/TextField"
 import  Button  from "@mui/material/Button"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lime, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F7B0FF',
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+});
 const CepPage = () => {
+
+ 
   const [cep, setCep] = useState('')
   const [dadosCep, setDadosCep] = useState(null)
   
@@ -22,28 +37,30 @@ const CepPage = () => {
   };
 
    return(
-    <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column' marginTop='15px' width='100vw' gap={2} marginBottom='100px'>
-      <Stack  alignItems='center' gap={1}>
-          <Typography variant="h4">Consulte seu CEP</Typography>
-          <Link href='https://brasilapi.com.br' target='_blank' underline="always"> via Brasil API</Link>
-      </Stack>
+    <ThemeProvider theme={theme}>
+      <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column' marginTop='15px' width='100vw' gap={2} marginBottom='100px'>
+        <Stack  alignItems='center' gap={1}>
+            <Typography variant="h4">Consulte seu CEP</Typography>
+            <Link href='https://brasilapi.com.br' target='_blank' underline="always"> via Brasil API</Link>
+        </Stack>
 
-      <Stack alignItems='center' gap={1} direction="row">
-          <TextField id="idCep" placeholder="00000000" required inputProps={{maxLength: 8}} value={cep} onChange={(event) => setCep(event.target.value)}></TextField>
-          <Button variant="contained" onClick={obterDadosCep} > Buscar </Button>
-      </Stack>
+        <Stack alignItems='center' gap={1} direction="row">
+            <TextField id="idCep" placeholder="00000000" required inputProps={{maxLength: 8}} value={cep} onChange={(event) => setCep(event.target.value)}></TextField>
+            <Button variant="contained" onClick={obterDadosCep} > Buscar </Button>
+        </Stack>
 
-    {dadosCep && 
-          <Stack width='50vh' gap={2} marginTop={1}>
-          <TextField label="Rua" fullWidth readOnly value={dadosCep.street}/>
-          <TextField label="Cidade" fullWidth readOnly value={dadosCep.city}/>
-          <TextField label="Estado" fullWidth readOnly value={dadosCep.state}/>
-          <TextField label="Latitude" fullWidth readOnly value={dadosCep.location.coordinates.latitude}/>
-          <TextField label="Longitude" fullWidth readOnly value={dadosCep.location.coordinates.longitude}/>
-      </Stack>
-    }
-    </Box>
-    
+      {dadosCep && 
+            <Stack width='50vh' gap={2} marginTop={1}>
+            <TextField label="Rua" fullWidth readOnly value={dadosCep.street}/>
+            <TextField label="Cidade" fullWidth readOnly value={dadosCep.city}/>
+            <TextField label="Estado" fullWidth readOnly value={dadosCep.state}/>
+            <TextField label="Latitude" fullWidth readOnly value={dadosCep.location.coordinates.latitude}/>
+            <TextField label="Longitude" fullWidth readOnly value={dadosCep.location.coordinates.longitude}/>
+        </Stack>
+      }
+      </Box>
+    </ThemeProvider>
+   
 
    )
 }
