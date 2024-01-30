3576d32cbc3b3705e6f1e4a5cb485d6df7c7cc2e
Francine Santos <francinesanto@gmail.com>
2024-01-30 08:33:31 -0300

    arrumando links footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,9 @@ import './styles.css'
 const Footer = () => {
     return (
         <footer>
-          <a href="">  <img src="/linkedin.svg" alt="" /> </a>
-          <a href="">  <img src="/github.svg" alt="" /> </a>
-          <a href="">  <img src="/email.svg" alt="" /> </a>
+          <a href="https://www.linkedin.com/in/francine-santos---/" target='_blank'>  <img src="/linkedin.svg" alt="" /> </a>
+          <a href="https://github.com/francinesanto"  target='_blank'>  <img src="/github.svg" alt="" /> </a>
+          <a href="mailto:francinesanto@gmail.com"  target='_blank'>  <img src="/email.svg" alt="" /> </a>
         </footer>
     )
 
